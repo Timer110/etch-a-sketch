7be83a4e9dbaf05163fa096c3dda54da38e69a61
Timer110 <121953691+Timer110@users.noreply.github.com>
2024-12-30 11:27:07 +0100

    first fully functional Version

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
 //general Variables
-let gridSize = 16;
+let gridSize = 40;
 let widthContent = 700;
 
 //HTMl-Variables
 const centerContent = document.querySelector(".centerContent");
+const resetButton = document.querySelector("button");
+
+
 
 function makeGrid(gridSize, widthContent) {
 
     let widthBox = (widthContent / gridSize) - 2 + "px";
-    console.log(widthBox);
+    let numberBoxes = gridSize * gridSize;
 
-    for (let i = 0; i < 256; i++) {
+    for (let i = 0; i < numberBoxes; i++) {
         const gridBox = document.createElement("div");
         gridBox.setAttribute("id", "gridBox" + i);
         gridBox.setAttribute("class", "gridBox");
         gridBox.style.height = widthBox;
         gridBox.style.width = widthBox;
         centerContent.appendChild(gridBox);
+
+        gridBox.addEventListener("mouseenter", () =>{
+        gridBox.style.backgroundColor = "black";
+        });
     }
 }
 
+function removeGrid() {
+
+    let boxes = document.querySelectorAll(".gridBox");
+
+    boxes.forEach(element => {
+        element.remove();
+    });
+
+}
+
+resetButton.addEventListener("click", () => {
+    let userInput = prompt("Please enter new grid-Size:", "40");
+    gridSize = userInput;
+    removeGrid();
+    makeGrid(gridSize, widthContent);
+
+
+});
+
+
 makeGrid(gridSize, widthContent);
-
-
-
-//change the girdSize doesnt work...
